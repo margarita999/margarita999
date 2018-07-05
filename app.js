b815211ba307a4e7de877e7a2ec4272e699c22be
root <root@localhost.localdomain>
2018-07-05 17:24:56 +0300

    commit

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function storeShops (allShops) {
 		if (err) throw err;
 	});
 	for (let i= 0; i< allShops.length; i++) {
-		client.query("INSERT INTO public.shop (content) VALUES('"+"HELLOO"+"'); ", (err, res) => {
+		client.query("INSERT INTO public.shop (content) VALUES('HELLOO'); ", (err, res) => {
 			if (err) throw err;
 		});
 	}
