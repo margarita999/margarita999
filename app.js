1dde328b2b9a697be07c0dac46e6d878da4292e5
root <root@localhost.localdomain>
2018-07-16 15:17:53 +0300

    Version 2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 // Database
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL //|| 'postgres://nqiqbdbukgygxb:cb3b5f7f38e2387e17257b6feebeb567d3d8932556f5943566b15ea590e6e5e9@ec2-54-228-251-254.eu-west-1.compute.amazonaws.com:5432/d2henp7bbv2obe';
+const connectionString = process.env.DATABASE_URL || 'postgres://nqiqbdbukgygxb:cb3b5f7f38e2387e17257b6feebeb567d3d8932556f5943566b15ea590e6e5e9@ec2-54-228-251-254.eu-west-1.compute.amazonaws.com:5432/d2henp7bbv2obe';
 /*
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -13,6 +13,11 @@ const client = new Client({
 
 //client.connect();
 */
+
+// Accepting POST Parameters
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 const PORT = process.env.PORT || 8081;
 
@@ -27,6 +32,122 @@ app.get('/', function (req, res) {
   res.send('Exal Backend Restful Service!');
 });
 
+app.post('/add/shop', async function (req, res) {
+	answer= await init ();
+	shop= new Shop ();
+
+	shop.AssistantID= req.body.AssistantID;
+	shop.username= req.body.username;
+	shop.password= req.body.password;
+	shop.name= req.body.name;
+	shop.companyRegistrationNumber= req.body.companyRegistrationNumber;
+	shop.phoneNumber= req.body.phoneNumber;
+	shop.email= req.body.email;
+	shop.address= req.body.address;
+	shop.typeOfPruducts= req.body.typeOfPruducts;
+	shop.language= req.body.language;
+	shop.keywords= req.body.keywords;
+	shop.picture= req.body.picture;
+
+	allShops.push(shop);
+
+	answer= await deleteAllShopsDB ();
+	answer= await storeShopsDB (allShops);
+
+	res.json({"Message" : "Success"});
+});
+
+app.post('/add/item', async function (req, res) {
+	answer= await init ();
+	item= new Item ();
+
+	item.ShopID= req.body.ShopID;
+	item.name= req.body.name;
+	item.category= req.body.category;
+	item.description= req.body.description;
+	item.size= req.body.size;
+	item.year= req.body.year;
+	item.keywords= req.body.keywords;
+	item.picture= req.body.picture;
+
+	allItems.push(item);
+
+	answer= await deleteAllItemsDB ();
+	answer= await storeItemsDB (allItems);
+
+	res.json({"Message" : "Success"});
+});
+
+app.post('/add/assistant', async function (req, res) {
+	answer= await init ();
+	assistant= new Assistant ();
+
+	assistant.fullName= req.body.fullName;
+	assistant.username= req.body.username;
+	assistant.password= req.body.password;
+	assistant.dateOfBirth= req.body.dateOfBirth;
+	assistant.phoneNumber= req.body.phoneNumber;
+	assistant.email= req.body.email;
+	assistant.address= req.body.address;
+	assistant.IBAN= req.body.IBAN;
+	assistant.operationLocation= req.body.operationLocation;
+	assistant.operationRange= req.body.operationRange;
+	assistant.language= req.body.language;
+	assistant.picture= req.body.picture;
+
+	allAssistants.push(assistant);
+
+	answer= await deleteAllAssistantsDB ();
+	answer= await storeAssistantsDB (allAssistants);
+
+	res.json({"Message" : "Success"});
+});
+
+app.get('/all/shops/', async function (req, res) {
+	answer= await init ();
+	res.json(allShops);
+});
+
+app.get('/all/items/', async function (req, res) {
+	answer= await init ();
+	res.json(allItems);
+});
+
+app.get('/all/assistants/', async function (req, res) {
+	answer= await init ();
+	res.json(allAssistants);
+});
+
+app.get('/all/shops/name-id', async function (req, res) {
+	answer= await init ();
+
+	shops= [];
+	for (let i= 0; i< allShops.length; i++) {
+		s= {};
+		s.name= allShops[i].name;
+		s.ID= allShops[i].ID;
+		shops.push(s);
+	}
+
+	res.json(shops);
+});
+
+app.get('/all/assistants/fullname-username-id', async function (req, res) {
+	answer= await init ();
+
+	assistants= [];
+	for (let i= 0; i< allAssistants.length; i++) {
+		a= {};
+		a.fullName= allAssistants[i].fullName;
+		a.username= allAssistants[i].username;
+		a.ID= allAssistants[i].ID;
+		assistants.push(a);
+	}
+
+	res.json(assistants);
+});
+
+/*
 app.get('/sources/:title/', function (req, res) {
 	let i;
 	for (i=0; i<Source.length; i++) {
@@ -38,13 +159,14 @@ app.get('/sources/:title/', function (req, res) {
 	}
   res.json({"Message" : "Error: There is no source with the requested Title."});
 });
+*/
 
 app.get('/test', async function (req, res) {
 
 	answer= await init ();
-	allShops= await retrieveShopsDB ();
-	allItems= await retrieveItemsDB ();
-	allAssistants= await retrieveAssistantsDB ();
+	//allShops= await retrieveShopsDB ();
+	//allItems= await retrieveItemsDB ();
+	//allAssistants= await retrieveAssistantsDB ();
 
 	S1= new Shop ();
 	S2= new Shop ();
@@ -70,10 +192,11 @@ app.get('/test', async function (req, res) {
   	res.json(answer);
 });
 
+/*
 app.get('/db', async function (req, res) {
-
   	res.send(connectionString);
 });
+*/
 
 app.listen(PORT, function () {
 	console.log('Example app listening on port' + PORT + '!');
@@ -83,6 +206,9 @@ var firstTime= true;
 var shopCount= 0;
 var itemCount= 0;
 var assistantCount= 0;
+var allShops= [];
+var allItems= [];
+var allAssistants= [];
 
 async function init () {
 	if (firstTime== false) {
@@ -106,6 +232,10 @@ async function init () {
 	assistantCount= res.rows.length;
 	
 	await client.end();
+
+	allShops= await retrieveShopsDB();
+	allItems= await retrieveItemsDB();
+	allAssistants= await retrieveAssistantsDB();
 	
 	firstTime= false;
 	return 0;
